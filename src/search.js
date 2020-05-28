29887b5ba72203fa5d2421cfdaa50fb94f8e395f
daviguelfi <daviguelfi@gmail.com>
2020-05-28 15:45:35 -0300

    style ajust

--- src/search.js
+++ src/search.js
@@ -2,7 +2,7 @@ import API_URL from './config';
 import toJSON from './utils';
 
 export const search = (query, type) => {
-  // eslint-disable-next-line no-undef 
+  // eslint-disable-next-line no-undef
   fetch(`${API_URL}/search?q=${query}&type=${type}`).then(toJSON);
 };
 export const searchArtists = (query) => {
